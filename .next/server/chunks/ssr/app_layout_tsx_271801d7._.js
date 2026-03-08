fb756bf5f2373db50b353967619bb61198805712
Developer <dev@setfarm.ai>
2026-03-09 02:08:09 +0300

    Remove build artifacts from feature branch

--- .next/server/chunks/ssr/app_layout_tsx_271801d7._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[33290,a=>{"use strict";var b=a.i(7997);function c({children:a}){return(0,b.jsx)("html",{lang:"en",children:(0,b.jsx)("body",{className:"antialiased",children:a})})}a.s(["default",()=>c,"metadata",0,{title:"Flappy Bird",description:"Flappy Bird game with Canvas engine"}])}];
-
-//# sourceMappingURL=app_layout_tsx_271801d7._.js.map
